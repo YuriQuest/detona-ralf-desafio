d8d7adcbc700de5864add013fc2ac9dfdcc4790f
YuriQuest <162909475+YuriQuest@users.noreply.github.com>
2024-12-04 12:53:02 -0300

    ✨ Feat: actions

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -6,11 +6,32 @@ const state = {
         score:      document.querySelector("#score"),
     },
     values: {
-        timerId:    null,
         gameVelocity: 1000,
         hitPosition: 0,
         result: 0,
+        currentTime: 60,
     },
+    actions:{
+        timerId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
+    }
+}
+
+function countDown(){
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+
+    if (state.values.currentTime < 0) {
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timerId);
+        alert("Fim de Jogo! O seu resultado foi: " + state.values.result);
+    }
+}
+
+function playSound() {
+    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume = 0.2;
+    audio.play()
 }
 
 function randomSquare(){
@@ -24,24 +45,20 @@ function randomSquare(){
     state.values.hitPosition = randomSquare.id;
 }
 
-function moveEnemy(){
-    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity)
-}
-
 function    addListenerHitBox(){
     state.view.squares.forEach((square) => {
         square.addEventListener("mousedown", ()=> {
             if(square.id === state.values.hitPosition) {
                 state.values.result++
                 state.view.score.textContent = state.values.result;
-                state.values.hitPosition = null
+                state.values.hitPosition = null;
+                playSound("hit");
             }
         })
     });
 }
 
 function init(){
-    moveEnemy();
     addListenerHitBox();
 };
 
